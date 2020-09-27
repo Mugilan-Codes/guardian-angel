b9155fe637ac87b406b7f3e6a036ffd3b0b1c984
Mugilan-Codes <mugilancodes@gmail.com>
2020-09-27 17:59:24 +0530

    Modified Async Storage

--- src/api/token.js
+++ src/api/token.js
@@ -2,12 +2,8 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 export const getToken = async () => {
   try {
-    const userData = await AsyncStorage.getItem('user_data');
-    const data = JSON.parse(userData);
-    console.log({ userData, data });
-    if (data !== null) {
-      return data;
-    }
+    const userData = await AsyncStorage.getItem('@user_data');
+    return userData != null ? JSON.parse('@user_data') : null;
   } catch (e) {
     console.log(`Getting Token Error: ${e}`);
     return null;
@@ -16,9 +12,29 @@ export const getToken = async () => {
 
 export const storeToken = async (user) => {
   try {
-    await AsyncStorage.setItem('user_data', JSON.stringify(user));
+    await AsyncStorage.setItem('@user_data', JSON.stringify(user));
   } catch (e) {
     console.log(`Storing Token Error: ${e}`);
     return null;
+  }
+};
+
+// 'user' or ['guardian']
+export const storeRole = async (role = 'guardian') => {
+  try {
+    await AsyncStorage.setItem('@user_role', role);
+  } catch (e) {
+    // saving error
+  }
+};
+
+export const getRole = async () => {
+  try {
+    const value = await AsyncStorage.getItem('@user_role');
+    if (value !== null) {
+      return value;
+    }
+  } catch (e) {
+    // error reading value
   }
 };
