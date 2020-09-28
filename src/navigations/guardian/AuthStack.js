37aab2e2a59c607460fd11af82648e45326e2c11
Mugilan-Codes <mugilancodes@gmail.com>
2020-09-28 19:56:49 +0530

    Create Auth Stacks to enable handle Login

---
+++ src/navigations/guardian/AuthStack.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from '../../screens/guardian/LoginScreen';
+import RegisterScreen from '../../screens/guardian/RegisterScreen';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Screen name='Register' component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
---
+++ src/navigations/guardian/HomeStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import GuardianHome from '../../screens/guardian/GuardianHome';
+
+const Stack = createStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={GuardianHome} />
+    </Stack.Navigator>
+  );
+};
+
+export default HomeStack;
