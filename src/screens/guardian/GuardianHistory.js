98db2e8053c837b40aac91e22b11612575180d9f
Mugilan-Codes <mugilancodes@gmail.com>
2020-09-28 16:43:42 +0530

    Get Guardian Location

--- src/screens/guardian/GuardianHistory.js
+++ src/screens/guardian/GuardianHistory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 const GuardianHistory = () => {
   return (
--- src/screens/guardian/GuardianMap.js
+++ src/screens/guardian/GuardianMap.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 const GuardianMap = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMessage('Permission needs to be given for map');
+      }
+
+      let location = await Location.getCurrentPositionAsync();
+      console.log(`Location of Guardian: ${JSON.stringify(location)}`);
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting...';
+  if (errorMessage) {
+    text = errorMessage;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View>
-      <Text>Map View</Text>
+      <Text>{text}</Text>
     </View>
   );
 };
