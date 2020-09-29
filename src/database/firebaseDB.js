22ebd82a83d56cccf17e772c409c6b29727fdb79
Mugilan-Codes <mugilancodes@gmail.com>
2020-09-29 14:58:48 +0530

    Take Photo and Upload

--- src/database/firebaseDB.js
+++ src/database/firebaseDB.js
@@ -4,5 +4,7 @@ import 'firebase/firestore';
 import { firebaseConfig } from '../../config';
 
 firebase.initializeApp(firebaseConfig);
+
 export const firebase_auth = firebase.auth();
 export const firestore = firebase.firestore();
+export const firebase_storage_ref = firebase.storage().ref();
--- src/screens/user/UserSubmit.js
+++ src/screens/user/UserSubmit.js
@@ -1,10 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext, useState } from 'react';
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Alert,
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+import { firebase_storage_ref } from '../../database/firebaseDB';
+import FormButton from '../../components/FormButton';
+import { windowHeight, windowWidth } from '../../utils/Dimensions';
+import { AuthContext } from '../../navigations/AuthProvider';
 
 const UserSubmit = () => {
+  const [image, setImage] = useState(null);
+  const [info, setInfo] = useState('');
+  const { user } = useContext(AuthContext);
+
+  const uploadImage = async (uri) => {
+    const res = await fetch(uri);
+    const blob = await res.blob();
+
+    const ref = firebase_storage_ref.child('images/test.jpg');
+    return ref.put(blob);
+  };
+
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync();
+    // console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const handleSubmit = async () => {
+    const res = await uploadImage(image);
+    const { bucket, fullPath } = res.metadata;
+    console.log(`gs://${bucket}/${fullPath}`);
+    Alert.alert('Success');
+  };
+
   return (
     <View style={styles.contatiner}>
-      <Text>Take Photo of Accident</Text>
+      <Text>{user.email}</Text>
+      {image && <Image source={{ uri: image }} style={styles.imageStyle} />}
+      <Button title='Take Photo' onPress={takePhoto} />
+
+      <TextInput
+        value={info}
+        style={styles.input}
+        placeholder='Enter Information'
+        placeholderTextColor='#666'
+        numberOfLines={3}
+        onChangeText={(textInfo) => setInfo(textInfo)}
+      />
+
+      <FormButton buttonTitle='Upload' onPress={handleSubmit} />
     </View>
   );
 };
@@ -14,6 +69,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageStyle: {
+    width: 300,
+    height: 300,
+    margin: 10,
+  },
+  input: {
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    width: windowWidth / 1.5,
+    height: windowHeight / 10,
+    fontSize: 16,
+    borderRadius: 8,
+    borderWidth: 1,
   },
 });
 
