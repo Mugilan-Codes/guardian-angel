cb0d3d40ac92cacbb832ff3361556c165808fd14
Mugilan-Codes <mugilancodes@gmail.com>
2020-10-09 18:17:20 +0530

    Integrated Maps in User

--- src/screens/user/UserSubmit.js
+++ src/screens/user/UserSubmit.js
@@ -7,6 +7,8 @@ import {
   View,
   TextInput,
   Alert,
+  Modal,
+  TouchableOpacity,
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
@@ -19,18 +21,27 @@ import {
 import FormButton from '../../components/FormButton';
 import { windowHeight, windowWidth } from '../../utils/Dimensions';
 import { AuthContext } from '../../navigations/AuthProvider';
+import MapView, { Callout } from 'react-native-maps';
 
 const UserSubmit = () => {
   const [image, setImage] = useState(null);
   const [info, setInfo] = useState('');
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [active, setActive] = useState(false);
+
   const { user } = useContext(AuthContext);
+
   const activeDocRef = firestore.collection('active').doc(user.email);
   const saverDocRef = firestore.collection('savers').doc(user.email);
 
   useEffect(() => {
     _requestCurrentLocation();
+
+    saverDocRef.onSnapshot((doc) => {
+      setActive(doc.data().active);
+    });
   }, []);
 
   const _requestCurrentLocation = async () => {
@@ -49,20 +60,20 @@ const UserSubmit = () => {
     const res = await fetch(uri);
     const blob = await res.blob();
 
-    const ref = firebase_storage_ref
+    const ref = await firebase_storage_ref
       .child(`images/${user.uid}/${new Date().toISOString()}.jpg`)
       .put(blob);
 
-    ref.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-      console.log(`Donwload URL = ${downloadURL}`);
-    });
+    // ref.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+    //   console.log(`Donwload URL = ${downloadURL}`);
+    // });
 
     return ref;
   };
 
   const takePhoto = async () => {
     let result = await ImagePicker.launchCameraAsync();
-    console.log({ result });
+    // console.log({ result });
 
     if (!result.cancelled) {
       setImage(result.uri);
@@ -73,6 +84,7 @@ const UserSubmit = () => {
     const res = await uploadImage(image);
     const { bucket, fullPath } = res.metadata;
     const photo_url = `gs://${bucket}/${fullPath}`;
+    console.log({ photo_url });
     await _requestCurrentLocation();
     console.log(latitude, longitude);
     await activeDocRef.set({
@@ -81,30 +93,68 @@ const UserSubmit = () => {
       info,
       accepted: false,
       completed: false,
+      timestamp: firebase_instance.firestore.FieldValue.serverTimestamp(),
       location: new firebase_instance.firestore.GeoPoint(latitude, longitude),
     });
     await saverDocRef.update({ active: true });
     setImage(null);
     setInfo('');
+    setModalVisible(!modalVisible);
   };
 
   // Create Modal for this update photo action
   return (
     <View style={styles.contatiner}>
-      <Text>{user.email}</Text>
-      {image && <Image source={{ uri: image }} style={styles.imageStyle} />}
-      <Button title='Take Photo' onPress={takePhoto} />
+      <Modal visible={modalVisible} animationType='slide'>
+        <View style={styles.contatiner}>
+          {/* <Text>{user.email}</Text> */}
+          {image && <Image source={{ uri: image }} style={styles.imageStyle} />}
+          <Button title='Take Photo' onPress={takePhoto} />
+          {/* <Text>
+            Latitutde: {latitude} - Longitude: {longitude}
+          </Text> */}
+          <TextInput
+            value={info}
+            style={styles.input}
+            placeholder='Enter Information'
+            placeholderTextColor='#666'
+            numberOfLines={3}
+            onChangeText={(textInfo) => setInfo(textInfo)}
+          />
 
-      <TextInput
-        value={info}
-        style={styles.input}
-        placeholder='Enter Information'
-        placeholderTextColor='#666'
-        numberOfLines={3}
-        onChangeText={(textInfo) => setInfo(textInfo)}
+          <FormButton buttonTitle='Upload' onPress={handleSubmit} />
+
+          <TouchableOpacity
+            style={styles.modalTouchable}
+            onPress={() => setModalVisible(false)}
+          >
+            <Text style={styles.modalTouchableText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude,
+          longitude,
+          latitudeDelta: 0.004,
+          longitudeDelta: 0.004,
+        }}
+        showsUserLocation
+        rotateEnabled={false}
+        mapType='hybrid'
+        onMapReady={_requestCurrentLocation}
       />
-
-      <FormButton buttonTitle='Upload' onPress={handleSubmit} />
+      {!active && (
+        <Callout style={styles.calloutBtn}>
+          <TouchableOpacity
+            style={styles.touchableContainer}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.touchableText}>Upload</Text>
+          </TouchableOpacity>
+        </Callout>
+      )}
     </View>
   );
 };
@@ -129,6 +179,34 @@ const styles = StyleSheet.create({
     fontSize: 16,
     borderRadius: 8,
     borderWidth: 1,
+  },
+  mapStyle: {
+    width: windowWidth,
+    height: windowHeight,
+  },
+  calloutBtn: {
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+    backgroundColor: '#3443df',
+  },
+  touchableContainer: {
+    padding: 10,
+  },
+  touchableText: {
+    fontSize: 24,
+    color: '#fff',
+  },
+  modalTouchable: {
+    padding: 10,
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    backgroundColor: '#3443e4',
+  },
+  modalTouchableText: {
+    fontSize: 20,
+    color: '#fffeee',
   },
 });
 
