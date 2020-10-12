b2de6083f5c072d3bbfb5b6413b6e1477081df51
ezhil-2012 <ezhil.simi@gmail.com>
2020-10-12 14:38:07 +0530

    Guardian History

--- src/screens/guardian/GuardianHistory.js
+++ src/screens/guardian/GuardianHistory.js
@@ -1,15 +1,23 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Image,
+} from "react-native";
 
-import { firestore } from '../../database/firebaseDB';
-import { AuthContext } from '../../navigations/AuthProvider';
+import { firestore, firebase_instance } from "../../database/firebaseDB";
+import { AuthContext } from "../../navigations/AuthProvider";
 
 const GuardianHistory = () => {
   const [history, setHistory] = useState([]);
+  const [downloadUrl, setDownloadUrl] = useState("");
 
   const { user } = useContext(AuthContext);
 
-  const docRef = firestore.collection('guardians').doc(user.email);
+  const docRef = firestore.collection("guardians").doc(user.email);
 
   useEffect(() => {
     docRef.onSnapshot((doc) => {
@@ -20,30 +28,63 @@ const GuardianHistory = () => {
   const renderItems = history ? (
     history.map((hist, idx) => {
       console.log(hist);
+      const ref = firebase_instance.storage().refFromURL(hist.photo_url);
+      ref.getDownloadURL().then(function (url) {
+        setDownloadUrl(url);
+      });
+      const logo = {
+        uri: downloadUrl,
+        width: 75,
+        height: 75,
+      };
       return (
-        <Text style={styles.text} key={idx}>
-          {hist.info}
-        </Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+            backgroundColor: "#fff",
+            elevation: 10,
+            margin: 10,
+            padding: 10,
+          }}
+          key={idx}
+        >
+          <View>
+            <Text style={{ color: "#333" }}>Email: {hist.user_email}</Text>
+            <Text style={{ color: "#333" }}>Info: {hist.info}</Text>
+          </View>
+          <View>
+            <Image source={logo} />
+          </View>
+        </View>
       );
     })
   ) : (
     <Text style={styles.text}>No history yet</Text>
   );
 
-  return <View style={styles.container}>{renderItems}</View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 40, textAlign: "center", margin: 10 }}>
+        Guardian History
+      </Text>
+      <ScrollView>{renderItems}</ScrollView>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#777',
   },
   text: {
     fontSize: 25,
-    fontStyle: 'italic',
-    color: '#333',
+    fontStyle: "italic",
+    color: "#333",
+    textAlign: "center",
+    marginTop: 50,
   },
 });
 
